5fee143c9b00b470f9b9e1f54619b93eab0b1b42
ninjaginja^C <mollywojcik@Mollys-MBP-2.domain>
2018-01-05 07:24:37 -0700

    buttons and still images working, but animated gifs pulling in random set of images

---
+++ assets/javascript/app.js
@@ -0,0 +1,109 @@
+window.onload = function() {
+
+  // Initial array of tvShows
+  var tvShows = ["Saved By The Bell", "Magnum PI", "Gilligan's Island", "Flight of the Concords"];
+
+  function displayGif() {
+    $("#results").empty();
+
+    var tvShow = $(this).attr("data-name");
+
+    // Giphy API key
+    var APIKey = "ZnfZOhi0lEUfCLKmbun9KhR0Diw6Wx1x";
+
+    // QueryURL for Giphy API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + tvShow + "&limit=10&lang=en";
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+
+      // Loop through results in object array
+      for (var i = 0; i < response.data.length; i++) {
+
+        // Create divs for each thumbnail and gif
+        var tvDiv = $("<div>");
+        tvDiv.addClass("col-md-4 tvShow");
+        var thumbnailDiv = $("<div>");
+        thumbnailDiv.addClass("thumbnail");
+        tvDiv.append(thumbnailDiv);
+
+        // Store rating in variable and add to p tag
+        var rating = response.data[i].rating;
+        var ratingUpperCase = rating.toUpperCase();
+        var pRating = $("<p>");
+        pRating.addClass("rating");
+        pRating.append("Rating: " + ratingUpperCase);
+        thumbnailDiv.append(pRating);
+
+        // Create img tag
+        var gif = $("<img class = 'gif'>");
+        gif.attr("data-state", "still");
+
+        // Store still image in variable and add to img tag
+        var stillImageUrl = response.data[i].images.fixed_width_still.url;
+        gif.attr("data-still", stillImageUrl);
+
+        // Store animated gif in variable with data-state
+        var animatedImageUrl = response.data[i].images.fixed_width.url;
+        gif.attr("data-animate", animatedImageUrl);
+
+        gif.attr("src", stillImageUrl);
+        thumbnailDiv.append(gif);
+
+        $("#results").append(tvDiv);
+
+      }
+    });
+  };
+
+  function displayButtons() {
+    $(".buttons").empty();
+
+    for (var i = 0; i < tvShows.length; i++) {
+      var button = $("<button>");
+      button.addClass("tvShow");
+      button.attr("data-name", tvShows[i]);
+      button.text(tvShows[i]);
+      $(".buttons").append(button);
+    }
+  }
+
+
+    $("#search-button").on("click", function(event) {
+      event.preventDefault();
+      // This line grabs the input from the textbox
+      var tvShow = $("#input").val().trim();
+
+      // Adding movie from the textbox to our array
+      tvShows.push(tvShow);
+
+      // Calling displayButtons which handles the processing of tvShows array
+      displayButtons();
+    });
+
+
+
+    // Adding a click event listener to all elements with a class of .tvShow, call displayGif function
+    $(document).on("click", ".tvShow", displayGif);
+
+
+    $(".gif").on("click", function() {
+      var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+        console.log("Hello");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+        console.log("Hello");
+      }
+    });
+
+
+    // Display buttons on page load
+    displayButtons();
+
+};
